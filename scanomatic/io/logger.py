f813c432fbe5f4ff5270a552b9022fbadc9da7b1
Martin Zackrisson <martin.zackrisson@molflow.com>
2021-10-29 11:52:08 +0200

    Remove some more covered things

--- scanomatic/io/logger.py
+++ scanomatic/io/logger.py
@@ -1,23 +1,19 @@
 from collections import defaultdict
-import datetime
-from io import StringIO
 import re
-import sys
-from typing import Optional, TextIO, Union
-import warnings
-from functools import partial
+from typing import Optional, Union
 import logging
-from scanomatic.io import backup
 from scanomatic.io.paths import Paths
 
 _FORMAT = '%(asctime)s -- %(levelname)s\t**%(name)s** %(msg)s'
 _DATEFMT = '%Y-%m-%d %H:%M:%S'
 _FORMATTER = logging.Formatter(fmt=_FORMAT, datefmt=_DATEFMT)
 _HANDLERS: dict[str, Union[logging.FileHandler, logging.StreamHandler]] = {}
+"""
 _USERS: defaultdict[
     logging.FileHandler,
     list[logging.Logger],
 ] = defaultdict(list)
+"""
 
 logging.basicConfig(
     Paths().log_ui_server,
@@ -51,70 +47,22 @@ def get_logger(name: str, file_path: Optional[str] = None) -> logging.Logger:
     if file_path is not None:
         handler = get_file_handler(file_path)
         logger.addHandler(handler)
-        _USERS[handler].append(logger)
+        # _USERS[handler].append(logger)
     return logger
 
 
-def rotate_log_target(logger: logging.Logger, path: str):
-    if path not in _HANDLERS and _HANDLERS[path] not in logger.handlers:
-        return
-
-    file_handler = _HANDLERS[path]
-    stream = StringIO()
-    stream_handler = logging.StreamHandler(stream)
-    stream_handler.setFormatter(_FORMATTER)
-    for l in _USERS[file_handler]:
-        l.addHandler(stream_handler)
-        l.removeHandler(file_handler)
-
-    file_handler.close()
-
-    backup.backup_file(path)
-
-    logger.removeHandler(stream_handler)
-
-    logger.addHandler(file_handler)
-
-
 def set_logging_target(logger: logging.Logger, path: str):
+    """
     while logger.handlers:
         logger.removeHandler(logger.handlers[0])
     handler = logging.FileHandler(path, encoding='utf-8')
     logger.addHandler(handler)
+    """
+    raise ValueError("Those loggers should be using get_logger...")
 
 
 class XLogger:
-    EXCEPTION = 0
-    CRITICAL = 1
-    ERROR = 2
-    WARNING = 3
-    INFO = 4
-    DEBUG = 5
-
     _LOGFORMAT = "%Y-%m-%d %H:%M:%S -- {lvl}\t**{name}** "
-
-    _DEFAULT_LOGFILE = None
-    _DEFAULT_CATCH_OUT = False
-    _DEFAULT_CATCH_ERR = False
-
-    _LOGLEVELS = [
-        (0, 'exception', 'EXCEPTION'),
-        (1, 'critical', 'CRITICAL'),
-        (2, 'error', 'ERROR'),
-        (3, 'warning', 'WARNING'),
-        (4, 'info', 'INFO'),
-        (5, 'debug', 'DEBUG')
-    ]
-
-    _LOGLEVELS_TO_TEXT = {}
-
-    def __init__(self, name, active=True):
-        self._level = self.INFO
-        self._log_file = None
-        self._loggerName = name
-        self._logLevelToMethod = {}
-        self._usePrivateOutput = False
-        self._suppressPrints = False
 
     @property
     def surpress_prints(self) -> bool:
