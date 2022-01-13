ca15c433ea1983d2ce3b8259b13a475348a21c07
Andreas Skyman <andreas.skyman@molflow.com>
2022-01-13 14:13:00 +0100

    downgrade d3

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: '[name].js',
     library: ['[name]'],
     libraryTarget: 'umd',
-    publicPath: '/js/som',
+    publicPath: '/js/somlib',
   },
   module: {
     rules: [
