d139bba1d753ae4924060c487f18136bc1258248
bengt.rydberg <bengt.rydberg@molflow.com>
2022-02-03 15:14:37 +0100

    better variable name

--- setup_config.py
+++ setup_config.py
@@ -10,10 +10,10 @@ CONFIG_DIR = "root/.scan-o-matic/config"
 def setup_config():
     if not os.path.isdir(CONFIG_DIR):
         os.makedirs(CONFIG_DIR)
-    for file in os.listdir(DEFAULT_CONFIG_DIR):
-        source_path = os.path.join(DEFAULT_CONFIG_DIR, file)
-        destination_path = os.path.join(CONFIG_DIR, file)
-        if not os.path.isfile(os.path.join(CONFIG_DIR, file)):
+    for filename in os.listdir(DEFAULT_CONFIG_DIR):
+        source_path = os.path.join(DEFAULT_CONFIG_DIR, filename)
+        destination_path = os.path.join(CONFIG_DIR, filename)
+        if not os.path.isfile(destination_path):
             copyfile(source_path, destination_path)
 
 
