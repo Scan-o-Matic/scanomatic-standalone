d0d9346b22d569a8116fd34ba6d433ade6c50214
bengt.rydberg <bengt.rydberg@molflow.com>
2022-02-04 10:28:44 +0100

    also copy file from subdirectores

--- setup_config.py
+++ setup_config.py
@@ -7,18 +7,22 @@ DEFAULT_CONFIG_DIR = "/tmp/data/config"
 CONFIG_DIR = "root/.scan-o-matic/config"
 
 
-def setup_config():
-    if not os.path.isdir(CONFIG_DIR):
-        os.makedirs(CONFIG_DIR)
-    for filename in os.listdir(DEFAULT_CONFIG_DIR):
-        source_path = os.path.join(DEFAULT_CONFIG_DIR, filename)
-        destination_path = os.path.join(CONFIG_DIR, filename)
+def setup_config(
+    default_config_dir: str, config_dir: str,
+):
+    if not os.path.isdir(config_dir):
+        os.makedirs(config_dir)
+    for filename in os.listdir(default_config_dir):
+        source_path = os.path.join(default_config_dir, filename)
+        destination_path = os.path.join(config_dir, filename)
         if (
             os.path.isfile(source_path)
             and not os.path.isfile(destination_path)
         ):
             copyfile(source_path, destination_path)
+        elif os.path.isdir(source_path):
+            setup_config(source_path, destination_path)
 
 
 if __name__ == "__main__":
-    setup_config()
+    setup_config(DEFAULT_CONFIG_DIR, CONFIG_DIR)
