e8a085617278c74dab997e22972c6fe70e31744d
joakim Möller <joakim.moller@molflow.com>
2022-02-11 16:36:54 +0100

    Split up stuatus enpoints

--- scanomatic/ui_server/status_api.py
+++ scanomatic/ui_server/status_api.py
@@ -6,47 +6,27 @@ from .general import convert_path_to_url, json_abort
 
 def add_routes(app: Flask, rpc_client: _ClientProxy):
 
-    @app.route("/api/status/<status_type>")
-    @app.route("/api/status/<status_type>/<status_query>")
-    def _status_api(status_type="", status_query=None):
+    @app.route("/api/status/server")
+    def status_server():
+        if rpc_client.online:
+            return jsonify(**rpc_client.get_status())
+        return json_abort(
+            503,
+            reason="System not yet ready",
+        )
 
-        if status_type == 'server':
-            if rpc_client.online:
-                return jsonify(success=True)
-            else:
-                return json_abort(
-                    503,
-                    reason="System not yet ready, probably starting up",
-                )
-        elif not rpc_client.online:
-            return jsonify(success=False, reason="Server offline")
-        elif status_type == 'queue':
+    @app.route("/api/status/queue")
+    def status_queue():
+        if rpc_client.online:
             return jsonify(queue=rpc_client.get_queue_status())
-        elif status_type == 'scanners':
-            if status_query is None or status_query.lower() == 'all':
-                return jsonify(scanners=rpc_client.get_scanner_status())
-            elif status_query.lower() == 'free':
-                return jsonify(
-                    scanners={
-                        s['socket']: s['scanner_name']
-                        for s in rpc_client.get_scanner_status()
-                        if 'owner' not in s or not s['owner']
-                    },
-                )
-            else:
-                try:
-                    return jsonify(
-                        scanner=next((
-                            s for s in rpc_client.get_scanner_status()
-                            if status_query in s['scanner_name']
-                        )),
-                    )
-                except StopIteration:
-                    return json_abort(
-                        400,
-                        reason=f"Unknown scanner or query '{status_query}'",
-                    )
-        elif status_type == 'jobs':
+        return json_abort(
+            503,
+            reason="System not yet ready",
+        )
+
+    @app.route("/api/status/jobs")
+    def status_jobs():
+        if rpc_client.online:
             data = rpc_client.get_job_status()
             for item in data:
                 if item['type'] == "Feature Extraction Job":
@@ -57,7 +37,38 @@ def add_routes(app: Flask, rpc_client: _ClientProxy):
                         item['log_file'],
                     )
             return jsonify(jobs=data)
-        elif status_type == 'server':
-            return jsonify(**rpc_client.get_status())
-        else:
-            return json_abort(reason='Unknown status request')
+        return json_abort(
+            503,
+            reason="System not yet ready",
+        )
+
+    @app.route("/api/status/scanners")
+    @app.route("/api/status/scanners/<status_query>")
+    def status_scanners(status_query=None):
+        if rpc_client.online:
+            if status_query is None or status_query.lower() == 'all':
+                return jsonify(scanners=rpc_client.get_scanner_status())
+            if status_query.lower() == 'free':
+                return jsonify(
+                    scanners={
+                        s['socket']: s['scanner_name']
+                        for s in rpc_client.get_scanner_status()
+                        if 'owner' not in s or not s['owner']
+                    },
+                )
+            try:
+                return jsonify(
+                    scanner=next((
+                        s for s in rpc_client.get_scanner_status()
+                        if status_query in s['scanner_name']
+                    )),
+                )
+            except StopIteration:
+                return json_abort(
+                    400,
+                    reason=f"Unknown scanner or query '{status_query}'",
+                )
+        return json_abort(
+            503,
+            reason="System not yet ready",
+        )
