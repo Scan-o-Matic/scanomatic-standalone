d283008c33103cce68b97e2af370e851f6332d51
Joakim Möller <joakim.moller@molflow.com>
2022-02-14 16:25:40 +0100

    remove g

--- scanomatic/ui_server/status_api.py
+++ scanomatic/ui_server/status_api.py
@@ -7,29 +7,26 @@ from .general import convert_path_to_url, json_abort
 blueprint = Blueprint('status_api', __name__)
 
 
-@blueprint.before_request
-def get_rpc():
-    if 'rpc' not in g:
-        g.rpc = get_client()
-
-
 @blueprint.route("/server")
 def status_server():
-    if g.rpc.online:
+    rpc_client = get_client()
+    if rpc_client.online:
         return jsonify(**g.rpc.get_status())
     return json_abort(503, reason="System not ready")
 
 
 @blueprint.route("/queue")
 def status_queue():
-    if g.rpc.online:
+    rpc_client = get_client()
+    if rpc_client.online:
         return jsonify(queue=g.rpc.get_queue_status())
     return json_abort(503, reason="System not ready")
 
 
 @blueprint.route("/jobs")
 def status_jobs():
-    if g.rpc.online:
+    rpc_client = get_client()
+    if rpc_client.online:
         data = g.rpc.get_job_status()
         for item in data:
             if item['type'] == "Feature Extraction Job":
@@ -46,7 +43,8 @@ def status_jobs():
 @blueprint.route("/scanners")
 @blueprint.route("/scanners/<status_query>")
 def status_scanners(status_query=None):
-    if g.rpc.online:
+    rpc_client = get_client()
+    if rpc_client.online:
         if status_query is None or status_query.lower() == 'all':
             return jsonify(scanners=g.rpc.get_scanner_status())
         if status_query.lower() == 'free':
